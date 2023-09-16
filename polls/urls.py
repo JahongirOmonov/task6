8dba72e08a062c6db8bd86ba54cfb1f7a1f56461
Jahongir <dodajoy0777@gmail.com>
2023-09-16 07:50:27 +0500

    last

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,37 @@
 from django.urls import path
-from .views import getjadval, postjadval, detail
+from .views import (GetAllJadval, 
+                    PostJadval, 
+                    GetDetailJadval,
+                      PatchJadval, 
+                      DeleteJadval,
+                        AllFunctionJadval,
+                          PostGetJadval)
+
+
+
+from .views import ( GetAllPhone, 
+                    GetDetailPhone, 
+                    PostPhone,
+                     PatchPhone,
+                     DeletePhone,
+                     AllFunctionPhone, 
+                    PostGetPhone)
 
 urlpatterns=[
-    path('all/', getjadval.as_view()),
-    path('detail/<int:myid>', detail),
-    path('create/>', postjadval.as_view())
+    path('GetAllJadval/', GetAllJadval.as_view()),
+    path('GetDetailJadval/<int:pk>', GetDetailJadval.as_view()),
+    path('PostJadval/', PostJadval.as_view() ),
+    path('PatchJadval/<int:pk>', PatchJadval.as_view()),
+    path('DeleteJadval/<int:pk>', DeleteJadval.as_view()),
+    path('PostGetJadval/', PostGetJadval.as_view()),
+    path('AllFunctionJadval/<int:pk>', AllFunctionJadval.as_view()),
+    path('GetAllPhone/', GetAllPhone.as_view()),
+    path('GetDetailPhone/<int:pk>', GetDetailPhone.as_view()),
+    path('PostPhone/', PostPhone.as_view() ),
+    path('PatchPhone/<int:pk>', PatchPhone.as_view()),
+    path('DeletePhone/<int:pk>', DeletePhone.as_view()),
+    path('PostGetPhone/', PostGetPhone.as_view()),
+    path('AllFunctionPhone/<int:pk>', AllFunctionPhone.as_view())
+
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.http import JsonResponse
 from .serializer import jadvalSerializer, phoneSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 
 # Create your views here.
 
@@ -16,29 +17,126 @@ from rest_framework.response import Response
 #     #         'name':i.name,
 #     #         'nechtaligi':i.nechtaligi
 #     #     })
-#     return JsonResponse(result.data, safe=False)
-class getjadval(APIView):
-    def get(self, request):
-        choose = jadval.objects.all()
-        yeah=jadvalSerializer(choose, many=True)
-        return Response(yeah.data)
+# #     return JsonResponse(result.data, safe=False)
+# class getjadval(APIView):
+#     def get(self, request):
+#         choose = jadval.objects.all()
+#         yeah=jadvalSerializer(choose, many=True)
+#         return Response(yeah.data)
+class GetAllJadval(generics.ListAPIView):
+    queryset=jadval.objects.all()
+    serializer_class=jadvalSerializer
+
+
+
+
+class GetDetailJadval(generics.RetrieveAPIView):
+    queryset = jadval.objects.all()
+    serializer_class=jadvalSerializer
+    # class getjadval(APIView):
+#     def get(self, request):
+#         choose = jadval.objects.all()
+#         yeah=jadvalSerializer(choose, many=True)
+#         return Response(yeah.data)
+
+class PostJadval(generics.CreateAPIView):
+    queryset=jadval.objects.all()
+    serializer_class=jadvalSerializer
+
+
+
+
+class PatchJadval(generics.UpdateAPIView):
+    queryset=jadval.objects.all()
+    serializer_class=jadvalSerializer
+
+class DeleteJadval(generics.DestroyAPIView):
+    queryset=jadval.objects.all()
+    serializer_class=jadvalSerializer
+
+
+    
+
+# def detail(request, myid):
+#     # each = phone.objects.get(id=myid)
+#     # data={'name':each.name, 'number':each.number}
+#     some = phone.objects.filter(id=myid)
+#     forgett = phoneSerializer(some, many=True)
+#     return JsonResponse(forgett.data, safe=False)
+
+class PostGetJadval(generics.ListCreateAPIView):
+    queryset=jadval.objects.all()
+    serializer_class=jadvalSerializer
+
+class AllFunctionJadval(generics.RetrieveUpdateDestroyAPIView):
+    queryset=jadval.objects.all()
+    serializer_class=jadvalSerializer
+
+
     
 
 
-def detail(request, myid):
-    # each = phone.objects.get(id=myid)
-    # data={'name':each.name, 'number':each.number}
-    some = phone.objects.filter(id=myid)
-    forgett = phoneSerializer(some, many=True)
-    return JsonResponse(forgett.data, safe=False)
+# def detail(request, myid):
+#     # each = phone.objects.get(id=myid)
+#     # data={'name':each.name, 'number':each.number}
+#     some = phone.objects.filter(id=myid)
+#     forgett = phoneSerializer(some, many=True)
+#     return JsonResponse(forgett.data, safe=False)
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+class GetAllPhone(generics.ListAPIView):
+    queryset=phone.objects.all()
+    serializer_class=phoneSerializer
 
 
-class postjadval(APIView):
-    def post(self, request):
-        boldi=request.data
-        serious = jadvalSerializer(data=boldi)
-        if serious.is_valid():
-            serious.save()
-            return Response(serious.data)
-        return Response(serious.errors)
+
+class GetDetailPhone(generics.RetrieveAPIView):
+    queryset = phone.objects.all()
+    serializer_class=phoneSerializer
+
+class PostPhone(generics.CreateAPIView):
+    queryset=phone.objects.all()
+    serializer_class=phoneSerializer
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+class PatchPhone(generics.UpdateAPIView):
+    queryset=phone.objects.all()
+    serializer_class=phoneSerializer
+
+class DeletePhone(generics.DestroyAPIView):
+    queryset=phone.objects.all()
+    serializer_class=phoneSerializer
+
+    
+# class postjadval(APIView):
+#     def post(self, request):
+#         boldi=request.data
+#         serious = jadvalSerializer(data=boldi)
+#         if serious.is_valid():
+#             serious.save()
+#             return Response(serious.data)
+#         return Response(serious.errors)
+
+class PostGetPhone(generics.ListCreateAPIView):
+    queryset=phone.objects.all()
+    serializer_class=phoneSerializer
+
+
+
+class AllFunctionPhone(generics.RetrieveUpdateDestroyAPIView):
+    queryset=phone.objects.all()
+    serializer_class=phoneSerializer
+
+
+# class postjadval(APIView):
+#     def post(self, request):
+#         boldi=request.data
+#         serious = jadvalSerializer(data=boldi)
+#         if serious.is_valid():
+#             serious.save()
+#             return Response(serious.data)
+#         return Response(serious.errors)
         
